11c9c2a2bb6972a3ce8e735ffd97021837210db3
Sebastián Gómez Dussán <nassud@gmail.com>
2022-03-04 23:16:19 -0500

    Adicionados componentes prime para formulario y tablas

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DialogModule } from "primeng/dialog";
 import { TableModule } from "primeng/table";
 import { ButtonModule } from "primeng/button";
+import { InputTextModule } from "primeng/inputtext";
+import { CardModule } from "primeng/card";
+import { MultiSelectModule } from "primeng/multiselect";
 
 @NgModule({
   declarations: [],
   imports: [],
-  exports: [TableModule, ButtonModule],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    DialogModule,
+    CardModule,
+    MultiSelectModule,
+  ],
 })
 export class SharedModule {}
--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { Employee } from "src/app/core/models";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Employee, Skill, SkillLevel } from "src/app/core/models";
 
 @Component({
   selector: "app-employees",
@@ -9,9 +10,35 @@ import { Employee } from "src/app/core/models";
 export class EmployeesComponent implements OnInit {
   employees!: Employee[];
 
+  newEmployee!: Employee;
+  skills!: Skill[];
+  selectedSkills!: Skill[];
+
+  form = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.minLength(3)]),
+    lastName: new FormControl("", [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    skills: new FormControl("", Validators.required),
+  });
+
   constructor() {}
 
   ngOnInit(): void {
+    this.skills = [
+      {
+        id: 1,
+        name: "Java",
+        level: SkillLevel.ADVANCED,
+      },
+      {
+        id: 2,
+        name: "PHP",
+        level: SkillLevel.INTERMEDIATE,
+      },
+    ];
+
     this.employees = [
       {
         id: 1,
@@ -20,5 +47,9 @@ export class EmployeesComponent implements OnInit {
         skills: [],
       },
     ];
+  }
+
+  submit() {
+    console.log(this.form.value);
   }
 }
